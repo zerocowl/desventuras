fa9e72d52ebdf717de350770dac35fa5803407e9
zerocowl <o_jp@hotmail.com>
2016-02-03 12:53:04 -0200

    auto completar com JS e ajax

---
+++ autoc_ajax.js
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+
+<script>
+$(function() {
+   $(document).ready(function(){
+var esportes = [ "Natação", "Futebol", "Vôlei", "Basquete" ];
+$("#esporte" ).autocomplete({ source: esportes }); });
+});
+})(jQuery);
+</script>
